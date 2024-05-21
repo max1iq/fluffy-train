444881e2be125df1bb625161abf0348d513238e5
max1iq <maxcool.coool@gmail.com>
2024-05-21 11:10:08 +0200

    DVD

--- src/scenes/Title.js
+++ src/scenes/Title.js
@@ -14,5 +14,22 @@ export default class Title extends Phaser.Scene {
             .setInteractive()
             .on('pointerdown', () => this.scene.start('levelOne'))
         button.setOrigin(0.5, 0.5)
+
+        this.player = this.physics.add.sprite(Math.random()*1200, Math.random()*600, 'player')
+        this.player.body.setAllowGravity(false)
+        this.player.body.setCollideWorldBounds(true)
+        this.player.body.setBounce(1, 1)
+        let startanglex = Math.random()
+        let startangley = Math.random()
+        if(startanglex > 0.5) {
+            this.player.body.setVelocityX(300)
+        }else{
+            this.player.body.setVelocityX(-300)
+        }
+        if(startangley > 0.5) {
+            this.player.body.setVelocityY(300)
+        }else{
+            this.player.body.setVelocityY(-300)
+        }
     }
 }
